e09825a7887021636b6f84aedb5c860c71a9b4e2
Rohit Thakare <thakarerohit.rt@gmail.com>
2020-11-07 23:27:55 +0530

    added redux, and implemented userReducer and useAction to header and app components

---
+++ src/redux/user/user-reducer.jsx
@@ -0,0 +1,28 @@
+
+//setting up intitial state of reducer
+
+const INITIAL_STATE = {
+    currentUser : null
+}
+
+
+//If state is never initiallised  
+//Then it will be assigned to initial state
+const userReducer = (state = INITIAL_STATE, action) => {
+
+    switch(action.type){
+        
+        case 'SET_CURRENT_USER' : 
+            return {
+                ...state,
+                currentUser : action.payload
+            }
+
+        default : 
+            return state
+        // If action dosent matches to our case then we will return state as it is
+    }
+}
+
+
+export default userReducer;
