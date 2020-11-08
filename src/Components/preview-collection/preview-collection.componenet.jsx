01a9d95c7e6803bec8c512c9c2b96f1ffb3c1534
Rohit Thakare <thakarerohit.rt@gmail.com>
2020-11-08 16:26:09 +0530

    updated shop data with new data , group items in a cart

--- src/Components/preview-collection/preview-collection.componenet.jsx
+++ src/Components/preview-collection/preview-collection.componenet.jsx
@@ -9,8 +9,8 @@ const PreviewCollection = ({ title, items }) => (
             {title.toUpperCase()}
         </h1>
         <div className="preview">
-            {items.filter((item, idx) => idx < 4).map(({id, ...otherItemProps})=>(
-                <CollectionItem key={id} {...otherItemProps} />
+            {items.filter((item, idx) => idx < 4).map((item)=>(
+                <CollectionItem key={item.id} item={item} />
             ))}
         </div>
     </div>
